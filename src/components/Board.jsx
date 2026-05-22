d1d133e06fd1fbe19207ae6ab62441d15c4cf369
Prashant Suthar <prashantsuthar.sp.00@gmail.com>
2026-05-22 11:40:24 +0530

    Create tic tac toe board component

---
+++ src/components/Board.jsx
@@ -0,0 +1,13 @@
+import Square from "./Square";
+
+const Board = ({ squares, onClick }) => {
+  return (
+    <div className="grid grid-cols-3 gap-3">
+      {squares.map((square, index) => (
+        <Square key={index} value={square} onClick={() => onClick(index)} />
+      ))}
+    </div>
+  );
+};
+
+export default Board;
